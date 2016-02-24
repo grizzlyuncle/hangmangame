087946fd64c64c6b6c209208d742c7b30f36a1ea
grizzlyuncle <smj300@hotmail.co.uk>
2016-02-24 15:59:11 +0000

    Creating the list
    Add functionality that allows the user to display a randomly generated
    city.

--- play.js
+++ play.js
@@ -1,1 +1,11 @@
-var cityArray = ["Birmigham", "Newcastle", "Durham"];
+function addElement(){
+    var cityArray = ["Durham", "Newcastle", "Manchester", "Edinburgh", "York"];
+    var ul = document.getElementById("listContainer");
+    var newListItem = document.createElement("li");
+    newListItem.id = "list-item";
+    newListItem.innerHTML = cityArray[Math.floor(Math.random()*cityArray.length)];
+    ul.appendChild(newListItem);
+
+    var currentDiv = document.getElementById("currentDiv");
+    document.body.insertBefore(ul, currentDiv);
+}
